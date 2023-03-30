e322fb56549ed749d003837cc2478e5f26c13597
Alexandre Cavalcante <ale.santos49@gmail.com>
2023-03-29 23:16:24 -0300

    Adicionado projeto básico para server com SignalR

---
+++ TaskProgressTracker/TaskServer.SignalServer/Hubs/TarefaHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace TaskServer.SignalServer.Hubs
+{
+    public class TarefaHub : Hub
+    {
+        public async IAsyncEnumerable<DateTime> Streaming(CancellationToken cancellationToken)
+        {
+            while (true)
+            {
+                yield return DateTime.Now;
+                await Task.Delay(1000, cancellationToken);
+            }
+        }
+    }
+}
