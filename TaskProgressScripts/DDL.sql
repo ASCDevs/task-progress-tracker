ea06bcb51e16f683e87d5c00ca0fc4f83d194e7c
Alexandre Cavalcante <ale.santos49@gmail.com>
2023-04-13 22:23:31 -0300

    Script DDL atualizado

--- TaskProgressScripts/DDL.sql
+++ TaskProgressScripts/DDL.sql
@@ -1,6 +1,6 @@
-CREATE TABLE tarefas(
+CREATE TABLE TasksTracker.dbo.tarefas(
 	id int not null identity(1,1),
-	id_tarefa VARCHAR(300) UNIQUE,
+	id_tarefa VARCHAR(300) null UNIQUE,
 	nm_tarefa VARCHAR(500),
 	dt_pedido_tarefa DATE,
 	dt_inicio_tarefa DATE,
