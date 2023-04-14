19ce38b9f26d8370a5e4a37394441ae096d683d8
Alexandre Cavalcante <ale.santos49@gmail.com>
2023-04-13 22:02:17 -0300

    DDL do banco em SQL Server

---
+++ TaskProgressScripts/DDL.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tarefas(
+	id int not null identity(1,1),
+	id_tarefa VARCHAR(300) UNIQUE,
+	nm_tarefa VARCHAR(500),
+	dt_pedido_tarefa DATE,
+	dt_inicio_tarefa DATE,
+	dt_fim_tarefa DATE,
+	status varchar(500)
+)
